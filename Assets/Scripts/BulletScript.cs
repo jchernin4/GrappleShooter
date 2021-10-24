ca3d934403d1d31f394fc352c6157f02bf90aaaa
jchernin4 <jeremychernin@gmail.com>
2021-10-24 00:12:42 -0400

    Fixed taking damage + formatting + deleting

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -2,33 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletScript : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class BulletScript : MonoBehaviour {
     public ParticleSystem impactSystem;
-    void Start()
-    {
 
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.layer == 6)
-        {
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.layer == 6) {
             return;
         }
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponentInChildren<PlayerController>().TakeDamage(25);
+
+        if (collision.gameObject.CompareTag("Player")) {
+            collision.gameObject.GetComponent<PlayerController>().TakeDamage(25);
         }
-        Vector3 pos = transform.forward.normalized;
-        Vector3 rot = -collision.contacts[0].normal;
+
+        /*Vector3 pos = transform.forward.normalized;
+        Vector3 rot = -collision.contacts[0].normal;*/
         ParticleSystem spawnedImpact = Instantiate(impactSystem, transform.position, Quaternion.Euler(90, 0, 0));
         Destroy(spawnedImpact, 1f);
         Destroy(gameObject, 0f);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -2,36 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunController : MonoBehaviour
-{
-    
+public class GunController : MonoBehaviour {
     public Transform player1;
-    Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
+    void Update() {
         transform.position = player1.position;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Plane plane = new Plane(Vector3.up, Vector3.zero);
         float distance;
-        if (plane.Raycast(ray, out distance))
-        {
+        if (plane.Raycast(ray, out distance)) {
             Vector3 target = ray.GetPoint(distance);
 
             Vector3 direction = target - transform.position;
             float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0, rotation, 0);
         }
-
-
 
         /* Vector3 mousePos = Input.mousePosition;
          mousePos.z = Camera.main.nearClipPlane;
